10b4b75f8ce4fc41672c60a048b408f3b32180bf
Pete Jones <pwj2012@gmail.com>
2021-01-22 10:00:15 -0800

    thing

--- macro_tracker/settings.py
+++ macro_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9(t6jspr78veatli(!sxojltmekli9ilk09msujbt&(o84c=ax'
+SECRET_KEY = 'i9(t6jspr78veatli(!sxojltmekli9ilk09msujbt&(o84c=ax'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
